2187b0a5754a25cca0eaf611dbcbb1608234f397
rebeccahong <rebeccayhong@gmail.com>
2018-10-09 09:46:41 -0700

    Add fields, questions, and answers

--- Lab01-AboutMeQuiz/Lab01-AboutMeQuiz/Program.cs
+++ Lab01-AboutMeQuiz/Lab01-AboutMeQuiz/Program.cs
@@ -6,7 +6,18 @@ namespace Lab01_AboutMeQuiz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program game = new Program();
+            game.questions = new string[5] { "In which city was I born?", "What's my favorite ice cream store in Capitol Hill?", "Which instrument did I play while growing up?", "Which sport did I play?", "Which weapon did I fence?" };
+            game.answers = new string[5] { "Flushing, NY", "Salt & Straw", "Flute", "Fencing", "Epee" };
+        }
+
+        private string[] questions;
+
+        private string[] answers;
+
+        static string[] PopulateQuestionsArray()
+        {
+
         }
     }
 }
