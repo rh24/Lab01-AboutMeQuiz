eb4c155ce3f6e96208d2f8ed8f22c056e24c63c3
rebeccahong <rebeccayhong@gmail.com>
2018-10-09 10:09:58 -0700

    Create game score bool array in Main method

--- Lab01-AboutMeQuiz/Lab01-AboutMeQuiz/Program.cs
+++ Lab01-AboutMeQuiz/Lab01-AboutMeQuiz/Program.cs
@@ -10,6 +10,7 @@ namespace Lab01_AboutMeQuiz
             Program game = new Program();
             game.questions = new string[5] { "In which city was I born?", "What's my favorite ice cream store in Capitol Hill?", "Which instrument did I play while growing up?", "Which sport did I play?", "Which weapon did I fence?" };
             game.answers = new string[5] { "Flushing, NY", "Salt & Straw", "Flute", "Fencing", "Epee" };
+            game.score = new bool[5];
 
         }
 
@@ -29,7 +30,7 @@ namespace Lab01_AboutMeQuiz
                 Console.WriteLine(questions[i]);
                 string userInput = Console.ReadLine();
                 int idx = i;
-                this.CheckAnswer(userInput, idx);
+                this.score[idx] = this.CheckAnswer(userInput, idx);
             }
         }
 
